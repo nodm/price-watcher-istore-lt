8ef37a53a6afcfc26c4e25ca291accb67b1d9b39
Dmytro Novikov <nodm.pe@gmail.com>
2022-10-22 22:01:31 +0300

    Add lambda for sending Telegram messages (#26)

--- src/functions/telegram-message-processor/handler.ts
+++ src/functions/telegram-message-processor/handler.ts
@@ -1,21 +1,48 @@
-import { SQSEvent, SQSHandler, SQSMessageAttributes, SQSRecord } from 'aws-lambda';
+import { Context, SQSEvent, SQSHandler, SQSRecord } from 'aws-lambda';
+import { SQS } from 'aws-sdk';
 import { Message } from '@grammyjs/types';
-import { sendMessage } from '../../services/telegram';
 
-const telegramMessageProcessor: SQSHandler = async (event: SQSEvent) => {
-  console.log('telegramMessageProcessor :: Event received', event);
+const sqs = new SQS();
 
-  await Promise.allSettled(event.Records.map((record: SQSRecord) => {
-    const messageAttributes: SQSMessageAttributes = record.messageAttributes;
-    console.log(
-      'telegramMessageProcessor :: Message Attributes',
-      JSON.stringify(messageAttributes, null, 2),
-    );
+const telegramMessageProcessor: SQSHandler = async (event: SQSEvent, context: Context) => {
+  console.log('telegramMessageProcessor :: Message received', event);
+  console.log('telegramMessageProcessor :: Context.invokedFunctionArn', context.invokedFunctionArn.split);
+
+  const [, , , region, accountId] = context.invokedFunctionArn.split(':');
+  const { TELEGRAM_OUTGOING_MESSAGE_QUEUE_NAME: queueName } = process.env;
+  const queueUrl: string = `https://sqs.${region}.amazonaws.com/${accountId}/${queueName}`
+
+  console.log('telegramMessageProcessor :: SNS URL', queueUrl);
+
+  const results = await Promise.allSettled(event.Records.map((record: SQSRecord) => {
     const message = JSON.parse(record.body) as Message;
-    const { chat: { id: chatId }, text } = message
+    const { chat: { id: chatId }, text } = message;
 
-    return sendMessage(`_${text}_ Hello, *${message?.from?.first_name}*!`, chatId);
+    return sqs.sendMessage({
+      QueueUrl: queueUrl,
+      MessageBody: JSON.stringify({
+        chatId,
+        text: `_${text}_ Hello *${message?.from?.first_name}*`
+      }),
+    }).promise();
   }));
+
+  for(const result of results) {
+    if (result.status === 'fulfilled') {
+      console.log(
+        'telegramMessageProcessor',
+        'status:', result.status,
+        'value:', result.value,
+      );
+      return;
+    }
+
+    console.log(
+      'telegramMessageProcessor',
+      'status:',result. status,
+      'reason:', result.reason,
+    );
+  }
 };
 
 export const main = telegramMessageProcessor;
