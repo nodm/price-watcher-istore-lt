948b2933eb24a244f487489eb9c27ae7c87e3962
Dmytro Novikov <nodm.pe@gmail.com>
2022-10-22 17:52:11 +0300

    Publish Telegram message to the SNS and send response

--- serverless.ts
+++ serverless.ts
@@ -2,6 +2,9 @@ import type { AWS } from '@serverless/typescript';
 
 import botWebhook from '@functions/bot-webhook';
 import priceWatcher from '@functions/price-watcher';
+import telegramMessageProcessor from '@functions/telegram-message-processor';
+
+// const ONE_HOUR = 60 * 60;
 
 const serverlessConfiguration: AWS = {
   service: 'price-watcher-istore-lt',
@@ -21,11 +24,52 @@ const serverlessConfiguration: AWS = {
     environment: {
       AWS_NODEJS_CONNECTION_REUSE_ENABLED: '1',
       NODE_OPTIONS: '--enable-source-maps --stack-trace-limit=1000',
+      TELEGRAM_MESSAGE_QUEUE_NAME: '${self:service}-${self:provider.stage}-telegram-message-queue',
+      // TELEGRAM_MESSAGE_DEAD_LETTER_QUEUE_NAME: '${self:service}-${self:provider.stage}-telegram-message-dead-letter-queue',
+    },
+    iam: {
+      role: {
+        statements: [
+          {
+            Effect: 'Allow',
+            Action: ['sqs:SendMessage'],
+            Resource: {
+              'Fn::GetAtt': ['TelegramMessageQueue', 'Arn'],
+            },
+          },
+        ],
+      },
     },
   },
   functions: {
     botWebhook,
     priceWatcher,
+    telegramMessageProcessor,
+  },
+  resources: {
+    Resources: {
+      TelegramMessageQueue: {
+        Type: 'AWS::SQS:Queue',
+        Properties: {
+          QueueName: '${self:provider.environment.TELEGRAM_MESSAGE_QUEUE_NAME}',
+          FifoQueue: false,
+          // VisibilityTimeout: 60,
+          // RedrivePolicy: {
+          //   deadLetterTargetArn: {
+          //     'Fn::GetAtt': ['TelegramMessageDeadLetterQueue', 'Arn'],
+          //   },
+          //   maxReceiveCount: 5,
+          // },
+        },
+      },
+      // TelegramMessageDeadLetterQueue: {
+      //   Type: 'AWS::SQS:Queue',
+      //   Properties: {
+      //     QueueName: '${self:provider.environment.TELEGRAM_MESSAGE_DEAD_LETTER_QUEUE_NAME}',
+      //     MessageRetentionPeriod: ONE_HOUR,
+      //   },
+      // },
+    },
   },
   package: { individually: true },
   custom: {
