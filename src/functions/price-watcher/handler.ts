8e008bc463f6e367bf72d67936c908024b92cc34
Dmytro Novikov <nodm.pe@gmail.com>
2022-10-31 19:19:19 +0200

    Send Telegram messages as HTML, send price change information

--- src/functions/price-watcher/handler.ts
+++ src/functions/price-watcher/handler.ts
@@ -2,7 +2,8 @@ import { Context, ScheduledHandler } from 'aws-lambda';
 import PricesService from '@services/PricesService';
 import ProductService from '@services/ProductService';
 import SQSService from '@services/SQSService';
-import { Product } from '@models/product';
+import { Product, ProductItem } from '@models/product';
+import { createTelegramMessage } from './helpers';
 
 const priceWatcher: ScheduledHandler = async (_, context: Context): Promise<void> => {
   const paths = process.env.PAGE_PATHS?.split(',');
@@ -10,14 +11,17 @@ const priceWatcher: ScheduledHandler = async (_, context: Context): Promise<void
 
   const chatId = parseInt(process.env.TELEGRAM_CHAT_ID);
   const { TELEGRAM_OUTGOING_MESSAGE_QUEUE_NAME: queueName } = process.env;
-  const sendMessage = SQSService.send(context);
+  const sendMessage = SQSService.send(context, queueName);
 
   const results = await Promise.allSettled(paths.map(url => PricesService.getPrices(url)
-      .then((products: Product[]) => Promise
-        .allSettled(products.map(product => ProductService.setPrice(product)))
-        .then(() => products))
+    .then((products: Product[]) => Promise.allSettled(products.map(product => ProductService.setPrice(product))))
+    .then(results => results.reduce((products, result) => {
+      if (result.status === 'rejected') return products;
+
+      return [...products, result.value];
+    }, [] as ProductItem[]))
     .then(createTelegramMessage)
-    .then(text => sendMessage(queueName, { chatId,  text }))
+    .then(text => sendMessage({ chatId,  text, parseMode: 'HTML' }))
   ));
 
   results
@@ -29,19 +33,3 @@ const priceWatcher: ScheduledHandler = async (_, context: Context): Promise<void
 };
 
 export const main = priceWatcher;
-
-const sanitizeText = (text: string): string => text
-  .replace(/-/g, '\\-')
-  .replace(/\(/g, '\\(')
-  .replace(/\)/g, '\\)')
-  .replace(/\./g, '\\.');
-
-const createTelegramMessage = (products: Product[]): string =>  products
-  .map(({ name, currentPrice, specialPrice, url }) => {
-    const prettyPrice = Math.round(currentPrice)
-      .toString()
-      .match(/(\d+?)(?=(\d{3})+(?!\d)|$)/g).join(' ');
-
-    return `[*_${sanitizeText(name)}_*](${url}) \\- ${specialPrice ? '👍' : ''} *${sanitizeText(prettyPrice)} €*`;
-  })
-  .join('\n\n');
--- src/services/SQSService.ts
+++ src/services/SQSService.ts
@@ -1,13 +1,14 @@
 import { Context } from 'aws-lambda';
 import { SQS } from 'aws-sdk';
 
+const sqs = new SQS();
+
 const SQSService = {
-  send: (context: Context) => (queueName: string, message: unknown) => {
-    const sqs = new SQS();
+  send: (context: Context, queueName: string) => {
     const [, , , region, accountId] = context.invokedFunctionArn.split(':');
     const queueUrl = `https://sqs.${region}.amazonaws.com/${accountId}/${queueName}`;
 
-    return sqs.sendMessage({
+    return (message: unknown) => sqs.sendMessage({
       QueueUrl: queueUrl,
       MessageBody: typeof message === 'string' ? message : JSON.stringify(message),
     }).promise();
