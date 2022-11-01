116f76bca96820b90139bf818031b8d6a0858980
Dmytro Novikov <nodm.pe@gmail.com>
2022-11-01 21:29:50 +0200

    Format price message (#42)

--- src/functions/istore-lt-price-watcher/helpers.ts
+++ src/functions/istore-lt-price-watcher/helpers.ts
@@ -7,13 +7,13 @@ const splitNumber = (value: number): string => value
 
 export const createTelegramMessage = (products: ProductItem[]): string =>  products
   .map(({ name, currentPrice, previousPrice, specialPrice, url }) => {
+    const messageTitle =  `<a href="${url}"><b>${TelegramService.encodeHtml(name)}</b></a>`;
+    const messagePrice = `<b>${splitNumber(Math.round(currentPrice))}</b>${specialPrice ? '👍' : ''}`
     const priceDelta = Math.round(currentPrice - previousPrice);
     const priceChange = priceDelta
       ? ` (${priceDelta > 0 ? '⬆️' : '⬇️'} <b><i>${splitNumber(Math.abs(priceDelta))})</i></b>`
       : '';
-    const messageTitle =  `<a href="${url}"><b>${TelegramService.encodeHtml(name)}</b></a>`;
-    const messagePrice = `<b>${splitNumber(Math.round(currentPrice))}</b>${specialPrice ? '👍' : ''}${priceChange}`
 
     return `${messageTitle} - ${messagePrice}${priceChange}`;
   })
-  .join('\n');
+  .join('\n\n');
