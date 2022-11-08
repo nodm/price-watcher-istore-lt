0824cdcf3193039b8c8c74dec9e5bc28729d9980
Dmytro Novikov <nodm.pe@gmail.com>
2022-11-08 22:20:17 +0200

    Setup Jest (#49)

---
+++ test/mocks/handlers.ts
@@ -0,0 +1,12 @@
+// import { rest } from 'msw';
+
+export const handlers = [
+  // rest.get('http://localhost/status', (_req, res, ctx) => {
+  //   return res(
+  //     ctx.status(200),
+  //     ctx.json({
+  //       status: 'Ok',
+  //     }),
+  //   );
+  // }),
+];
