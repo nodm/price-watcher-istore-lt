75853430d8bf5ed1a117f8454544d2ba2d1e2e93
Dmytro Novikov <nodm.pe@gmail.com>
2022-11-19 17:56:31 +0200

    Increase finction timeout, store issue date as a string

--- src/functions/post-lt-philately-watcher/helpers.ts
+++ src/functions/post-lt-philately-watcher/helpers.ts
@@ -1,23 +1,22 @@
 import { PhilatelyProduct } from '@models/philatelyProduct';
 import TelegramService from '@services/TelegramService';
 
-export const createTelegramMessage = (product: PhilatelyProduct): string => {
+export const createTelegramMessage = (product: PhilatelyPuroduct): string => {
     const title = `<a href="${product.href}"><b>${TelegramService.encodeHtml(product.title)}</b></a>`;
-    const type = `<b>${product.type}</b>`;
+    const type = `<i>${product.type}</i>`;
     const year = product.year && `<b>${product.year}</b>`;
-    const dateOfIssue = product.dateOfIssue && `<b>Date:</b> <i>${product.dateOfIssue.substring(0, 10)}</i>`;
+    const dateOfIssue = product.dateOfIssue && `<b>Date:</b> <i>${product.dateOfIssue}</i>`;
     const catalogNumber = product.catalogNumber && `<b>No.</b> <i>${product.catalogNumber}</i>`;
-    const price = product.price && `<i>${product.price.value} ${product.price.currency}</i>`;
+    const price = product.price && `<b>Price:</b> <i>${product.price.value} ${product.price.currency}</i>`;
     const meta = product.meta.length && `<i>${TelegramService.encodeHtml(product.meta.join('\n'))}</i>`;
     const description = product.description.length &&
       `<b>Description:</b>\n${TelegramService.encodeHtml(product.description.join('\n'))}`;
 
+
     return [
         title,
-        type,
         year,
-        dateOfIssue,
-        catalogNumber,
+        [dateOfIssue, type, catalogNumber].filter(Boolean).join('&mbsp;'),
         price,
         meta,
         description,
